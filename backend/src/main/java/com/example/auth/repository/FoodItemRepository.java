16c40af7737abf4e287b7c8a5d5992b299543db8
SPARK INC <sathish.s.it25@psvpec.in>
2025-01-06 09:25:27 +0530

    Delete backend directory

--- backend/src/main/java/com/example/auth/repository/FoodItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.auth.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.auth.model.FoodItem;
-
-public interface FoodItemRepository extends JpaRepository<FoodItem, Long> {
-}
-
